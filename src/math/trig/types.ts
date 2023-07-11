49d8d44bd51250adb14d60f45cb08bdbbf7a0828
Samantha <contact@samanthanguyen.me>
2023-07-11 11:50:34 -0500

    chore: fix ESLint errors

--- src/math/trig/types.ts
+++ src/math/trig/types.ts
@@ -1,8 +1,8 @@
-export type SideTwo = { side2: number; }
-export type SideThree = { side3: number; }
-export type AngleOne = { angle1: number; }
-export type AngleTwo = { angle2: number; }
-export type AngleThree = { angle3: number; }
-export type OneAngleTwoSides = AngleThree & SideTwo & SideThree
-export type OneSideTwoAngles = SideThree & AngleTwo & AngleThree
-export type ThreeAngles = AngleOne & AngleTwo & AngleThree
+export type SideTwo = { side2: number; };
+export type SideThree = { side3: number; };
+export type AngleOne = { angle1: number; };
+export type AngleTwo = { angle2: number; };
+export type AngleThree = { angle3: number; };
+export type OneAngleTwoSides = AngleThree & SideTwo & SideThree;
+export type OneSideTwoAngles = SideThree & AngleTwo & AngleThree;
+export type ThreeAngles = AngleOne & AngleTwo & AngleThree;
