8fdc17322b52b957d265758f4448a8e7810b4680
Samantha <contact@samanthanguyen.me>
2023-10-31 00:04:18 -0500

    chore: drop CJS/UMD, move to full ESM (#37)

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,11 @@ export default defineConfig({
 	build: {
 		lib: {
 			entry: path.resolve(__dirname, 'src/index.ts'),
-			name: 'neoncitylights_tstd',
-			fileName: (format) => `neoncitylights_tstd.${format}.js`
-		}
+			name: 'tstd',
+			formats: ['es'],
+		},
 	},
 	plugins: [
-		dts()
-	]
+		dts(),
+	],
 });
